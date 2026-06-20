42d6d59f6abe571d5e348cac3dcb8a93e8ea1bfa
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-06-20 14:58:09 +0200

    creating api end point validating login api end point included json web tokens to store user date privatly used implemented token to cookies to keep requst dynamic and secure added a test registation api end point for test to store dev(demo) data and validate login end points

--- controllers/login.js
+++ controllers/login.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const loginRouter = express.Router();
 const bcrypt = require("bcryptjs");
-const userData = require("../database");
+const userData = require("../modules/studentUser");
+const jwt = require("jsonwebtoken");
 //
 /*
 passwords :
@@ -29,7 +30,7 @@ loginRouter.post("/", validateReqBody, async (req, res) => {
     const body = req.body;
     const email = body.email;
     const password = body.password;
-    const requst = await userData.find("email", email);
+    const requst = await userData.find({ email: email });
     if (requst.length === 0)
       return res.status(404).json({ ok: false, message: "No records found" });
     if (requst[0].role.code !== process.env.STUDENT_CODE)
@@ -40,31 +41,58 @@ loginRouter.post("/", validateReqBody, async (req, res) => {
       password,
       requst[0].password,
     );
-    if (isPasswordCorrect)
-      return res
-        .status(200)
-        .json({ ok: true, message: "success", userId: requst[0]._id });
+    if (isPasswordCorrect) {
+      const tokenData = {
+        email: requst[0].email,
+        _id: requst[0]._id,
+      };
+      const token = await jwt.sign(tokenData, process.env.ACCESS_TOKEN, {
+        expiresIn: "30d",
+      });
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        path: "/",
+        maxAge: 1000 * 60 * 60 * 24 * 30, //up untill 30 days(a month)
+      });
+      return res.status(200).json({ ok: true, message: "success" });
+    }
     res.status(401).json({ ok: false, message: "wrong password" });
   } catch (error) {
     res.status(500).json({ ok: false, message: `server error: ${error} ` });
   }
 });
 //
-loginRouter.get("/validate/:id", async (req, res) => {
+loginRouter.get("/validate/", async (req, res) => {
   try {
-    const userId = req.params.id;
-    if (!userId)
+    const token = req.cookies.token;
+    if (!token)
+      return res.status(403).json({
+        ok: false,
+        message: "access denied user not validated pls login",
+      });
+    const tokenData = await jwt.verify(token, process.env.ACCESS_TOKEN);
+    if (!tokenData)
       return res
-        .status(303)
-        .json({ ok: false, message: "access denied invalide requst id" });
-    const requst = await userData.find("_id", userId);
+        .status(403)
+        .json({ ok: false, message: "invalied session token pls login" });
+    const requst = await userData.find({ _id: tokenData._id });
     if (requst.length === 0)
       return res.status(404).json({ ok: false, message: "no records found" });
     if (requst[0].role.code !== process.env.STUDENT_CODE)
       return res
         .status(303)
         .json({ ok: false, message: "access denied  user not verified" });
-    res.status(200).json({ ok: true, message: "requst granted" });
+    const respondsData = {
+      firstName: requst[0].firstName,
+      lastName: requst[0].lastName,
+      email: requst[0].email,
+      dateOfBirth: requst[0].dateOfBirth,
+    };
+    res
+      .status(200)
+      .json({ ok: true, message: "requst granted", userInfo: respondsData });
   } catch (error) {
     res.status(500).json({ ok: false, message: `server error: ${error} ` });
   }
--- server.js
+++ server.js
@@ -2,18 +2,30 @@ const express = require("express");
 const server = express();
 require("dotenv").config();
 const cors = require("cors");
+const cookieparesr = require("cookie-parser");
+server.use(cookieparesr());
 server.use(
   cors({
     origin: [process.env.FRONTEND_URL_DEV, process.env.FRONTEND_URL],
+    credentials: true,
   }),
 );
+const moongose = require("mongoose");
 server.use("/api/paystack/webhook", express.raw({ type: "application/json" }));
 server.use(express.json());
 const port = process.env.SERVER_PORT || 5000;
 //
-server.listen(port, () => {
-  console.log(`server runing on port ${port}`);
-});
+moongose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log("database up and runing");
+    server.listen(port, () => {
+      console.log(`server runing on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 //test
 server.get("/", async (req, res) => {
   res.status(200).json({ ok: true });
