bed8b34ad545e2fa5ecfbcdda0c30ddb3178e806
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-06-22 15:57:24 +0200

    modifing socket api added middleware to validate user and client

--- controllers/socket.js
+++ controllers/socket.js
@@ -1,13 +1,35 @@
 const { Server } = require("socket.io");
+const userValidation = require("jsonwebtoken");
+
+//middleware
+function validateUser(socket) {
+  const token = socket.handshake.headers.cookie.split("=")[1];
+  const isUser = userValidation.verify(token, process.env.ACCESS_TOKEN);
+  if (!isUser) return socket.disconnect();
+}
+function validateClient(socket) {
+  const clientId = socket.handshake.headers["x-frontend-key"];
+  if (clientId !== process.env.CLIENT_KEY) return socket.disconnect();
+}
+//
 async function SocketConnection(serverPort) {
   const io = new Server(serverPort, {
     cors: {
       origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_DEV],
+      credentials: true,
     },
   });
   //
-  io.on("connection", (socket) => {
-    console.log(socket.id);
+  io.on("connection", async (socket) => {
+    try {
+      //middleware
+      validateUser(socket);
+      validateClient(socket);
+      //
+      console.log(socket.id);
+    } catch (error) {
+      console.log(error);
+    }
   });
 }
 module.exports = SocketConnection;
