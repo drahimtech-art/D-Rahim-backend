983bca2e47d2b278dc38fda29f3ac087255555ab
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-06-23 22:44:49 +0200

    modifing socket router created chat history to be saved on database when sent

--- controllers/socket.js
+++ controllers/socket.js
@@ -1,5 +1,6 @@
 const { Server } = require("socket.io");
 const userValidation = require("jsonwebtoken");
+const contactMessages = require("../modules/contactMessage");
 
 //middleware
 function validateUser(socket) {
@@ -10,6 +11,78 @@ function validateUser(socket) {
 function validateClient(socket) {
   const clientId = socket.handshake.headers["x-frontend-key"];
   if (clientId !== process.env.CLIENT_KEY) return socket.disconnect();
+}
+// save chat to db func
+async function saveForUserChatToDB(connectionId, contactId, message) {
+  try {
+    const doesChatExist = await contactMessages.find({
+      connectionId: connectionId,
+      contactId: contactId,
+    });
+    if (doesChatExist.length === 0) {
+      const createChatData = new contactMessages({
+        connectionId: connectionId,
+        contactId: contactId,
+        messages: [message],
+      });
+      const saveData = await createChatData.save();
+      if (saveData) {
+        console.log("Chat history created succesfull");
+      } else {
+        console.log("Chat history creation failed");
+      }
+    } else {
+      const oldMessages = doesChatExist[0].messages;
+      const newMessages = [...oldMessages, message];
+      const updateChat = await contactMessages.findOneAndUpdate(
+        { connectionId: connectionId, contactId: contactId },
+        { messages: newMessages },
+      );
+      if (updateChat) {
+        console.log("messages succesfull saved in chat history");
+      } else {
+        console.log("messages falied to save in chat history");
+      }
+    }
+  } catch (error) {
+    console.log(`server error: ${error}`);
+  }
+}
+//
+async function saveForConatactChatToDB(connectionId, contactId, message) {
+  try {
+    const doesChatExist = await contactMessages.find({
+      connectionId: contactId,
+      contactId: connectionId,
+    });
+    if (doesChatExist.length === 0) {
+      const createChatData = new contactMessages({
+        connectionId: contactId,
+        contactId: connectionId,
+        messages: [message],
+      });
+      const saveData = await createChatData.save();
+      if (saveData) {
+        console.log("Chat history created succesfull");
+      } else {
+        console.log("Chat history creation failed");
+      }
+    } else {
+      const oldMessages = doesChatExist[0].messages;
+      const newMessages = [...oldMessages, message];
+      const updateChat = await contactMessages.findOneAndUpdate(
+        { connectionId: contactId, contactId: connectionId },
+        { messages: newMessages },
+      );
+      if (updateChat) {
+        console.log("messages succesfull saved in chat history");
+      } else {
+        console.log("messages falied to save in chat history");
+      }
+    }
+  } catch (error) {
+    console.log(`server error: ${error}`);
+  }
 }
 //
 async function SocketConnection(serverPort) {
@@ -31,7 +104,10 @@ async function SocketConnection(serverPort) {
         socket.join(room);
       });
       socket.on("send-message", (messages, room) => {
-        console.log(room);
+        const connectionId = messages.from;
+        const contactId = messages.to;
+        saveForUserChatToDB(connectionId, contactId, messages);
+        saveForConatactChatToDB(connectionId, contactId, messages);
         socket.to(room).emit("receive-message", messages);
       });
     } catch (error) {
--- modules/contactMessage.js
+++ modules/contactMessage.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const messageSchema = new Schema({
-  userId: {
+  connectionId: {
     type: String,
     required: true,
   },
