e4eab5a8383abb09e9da4c6e530415aab6e714f4
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-06-24 17:17:13 +0200

    modifing apis database schemas and responds data on userconnections contact message added index on used query for fast query and responds when database scalls and modified connectAndchat router (getmessage) to limit requst to 50 collection and added a filter function to return only messages

--- modules/contactMessage.js
+++ modules/contactMessage.js
@@ -9,18 +9,18 @@ const messageSchema = new Schema({
     type: String,
     required: true,
   },
-  messages: [
-    {
-      from: { type: String, required: true },
-      to: { type: String, required: true },
-      type: { type: String, required: true },
-      imgUrl: { type: String, required: false },
-      date: { type: String, required: true },
-      time: { type: String, required: true },
-      text: { type: String, required: false },
-    },
-  ],
+  messages: {
+    from: { type: String, required: true },
+    to: { type: String, required: true },
+    type: { type: String, required: true },
+    imgUrl: { type: String, required: false },
+    date: { type: String, required: true },
+    time: { type: String, required: true },
+    text: { type: String, required: false },
+  },
+  createdAt: { type: Date, default: Date.now },
 });
+messageSchema.index({ connectionId: 1, contactId: 1, createdAt: -1 });
 const connnectionMessageSchema = mongoose.model(
   "connectionMassages",
   messageSchema,
