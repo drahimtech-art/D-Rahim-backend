c60a03df8944da775eb950da04f1066d9f90fd5d
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-07-09 01:04:39 +0200

    refactoring removed manul nested lop for date caculation and just suptracted old date from new date to get days passed less lines of code more reliable as manul logic calculate every hour of a day to get the hole days passed it has a big o(n) latancy because its a liner search yeah its takes one step at a time

--- controllers/feed/decayAlgorithim.js
+++ controllers/feed/decayAlgorithim.js
@@ -1,104 +1,6 @@
 function getPostAge(post) {
-  const data = { ...post };
-  const postDate = data.createdAt.toISOString().split("T");
-  const [postYear, postMonth, postDay] = postDate[0].split("-");
-  const [postHour, postMinites, postSeconds] = postDate[1].split(":");
-  const currentDate = new Date();
-  const currentYear = currentDate.getFullYear();
-  const currentMonth = currentDate.getMonth() + 1;
-  const currentDay = currentDate.getDate();
-  const currentHour = currentDate.getHours();
-  const currentMinites = currentDate.getMinutes();
-  let daysPassed = 0;
-  let hourPosted = Number(postHour);
-  let hourPassed = 0;
-  let postedYear = Number(postYear);
-  let postedMonth = Number(postMonth);
-  let postedDay = Number(postDay);
-  const thisYear = currentYear;
-  const thisMonth = currentMonth;
-  const thisDay = currentDay;
-  const thisHour = currentHour === 0 ? 24 : currentHour;
-  let pass = true;
-  //
-  for (let i = 0; pass === true; i++) {
-    //break point
-    if (
-      postedYear === thisYear &&
-      postedMonth === thisMonth &&
-      postedDay == thisDay &&
-      hourPosted === thisHour
-    ) {
-      pass = false;
-      break;
-    }
-    //
-    if (hourPosted === 24) {
-      // if incremented post hour is equal 1 day
-      hourPosted = 0; // reset hour (0:00)
-      let monthLenth = 0; // validate which month we are
-      if (
-        postedMonth === 4 ||
-        postedMonth === 6 ||
-        postedMonth === 9 ||
-        postedMonth === 11
-      ) {
-        //if we are in month with 30 days
-        monthLenth = 30;
-      } else if (postedMonth === 2) {
-        // if we are in febuwary month lenth is 28 days or 29 if leep year
-        // check if true is equal leep year
-        if (
-          postedYear % 4 === 0 &&
-          (postedYear % 100 !== 0 || postedYear % 400 === 0)
-        ) {
-          monthLenth = 29;
-        } else {
-          monthLenth = 28;
-        }
-      } else {
-        // other months are 31 days
-        monthLenth = 31;
-      }
-      //
-      if (postYear === thisYear) {
-        // check if on the same year
-        if (postedDay === monthLenth) {
-          // if day lenth is equal to days in month
-          postedDay = 1; // 1st day of next month
-          postedMonth += 1; // next month
-        } else {
-          // else keep counting days
-          postedDay += 1;
-        }
-      } else {
-        if (postedDay === monthLenth && postedMonth === 12) {
-          // if day lenth is equal to days in month and month lenth is equal 12 rest to next year
-          postedDay = 1; // 1st day of next month
-          postedMonth = 1; // next month
-          postedYear += 1;
-        } else if (postedDay === monthLenth) {
-          // if day lenth is equal to days in month and month lenth is not equal 12
-          postedDay = 1; // 1st day of next month
-          postedMonth += 1; // next month
-        } else {
-          // else keep counting days
-          postedDay += 1;
-        }
-      }
-    } else {
-      // if incremented post hour is not equal 1 day
-      hourPosted += 1; // keep counting hours
-    }
-    //
-    if (hourPassed === 24) {
-      // if hour passed is equal 1 day
-      hourPassed = 0; // reset hour (0:00)
-      daysPassed += 1; // add days passed + 1
-    } else {
-      hourPassed += 1; // else keep counting hours
-    }
-  }
+  const daysPassed =
+    (new Date() - new Date(post.createdAt)) / (1000 * 60 * 60 * 24);
   return daysPassed;
 }
 
@@ -141,7 +43,6 @@ function decayStats(p, gc, fc, uc) {
       totalScore *= 0.8;
     }
   }
-  console.log(`${totalScore}: total`);
   const result = {
     ...post,
     totalScore: Math.max(totalScore, 0.1),
--- controllers/feed/postFeeds.js
+++ controllers/feed/postFeeds.js
@@ -1,49 +1,88 @@
 const express = require("express");
 const postFeeds = express.Router();
 const feedsPosts = require("../../modules/feeds/post.js");
+const multer = require("multer");
+const path = require("path");
 const { randomUUID } = require("crypto");
 //middlewares
 const apiRequstValidation = require("../../middlewares/apiValidation.js");
 const userValdation = require("../../middlewares/userValidation.js");
-
 //
-postFeeds.post("/content", async (req, res) => {
-  try {
-    const contentBody = req.body;
-    //const userId = contentBody.userId;
-    const connectionId = contentBody.connectionId;
-    const hashTages = contentBody.hashTages;
-    const contentCaption = contentBody.caption;
-    const contentType = "image";
-    const postData = {
-      connectionId: connectionId,
-      engament: {
-        likes: 0,
-        comments: 0,
-        shares: 0,
-      },
-      content: {
-        type: contentType,
-        caption: contentCaption,
-        content: "https://cloud/someimage",
-      },
-      engamentStates: {
-        likesId: [],
-        comments: [],
-      },
-      postId: `${connectionId}$${randomUUID()}`,
-      hashTages: [...hashTages],
-      date: "2024/02/28",
-      time: "02:54",
-      createdAt: new Date(),
-    };
-    const createPost = new feedsPosts(postData);
-    const savePost = await createPost.save();
-    if (!savePost) throw new Error(savePost);
-    res.status(201).json({ ok: true, message: "succesful", post: savePost });
-  } catch (error) {
-    res.status(500).json({ ok: false, message: `server error: ${error}` });
-  }
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "storage/feedsContent");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}${path.extname(file.originalname)}`);
+  },
 });
+const uploadMiddleware = multer({ storage: storage });
+//
+postFeeds.post(
+  "/content",
+  apiRequstValidation,
+  userValdation,
+  uploadMiddleware.single("media"),
+  async (req, res) => {
+    try {
+      const body = JSON.parse(req.body.postContent);
+      if (!body)
+        return res.status(400).json({
+          ok: false,
+          message: "invalid requst body post content not found",
+        });
+      if (
+        !body.connectionId ||
+        !body.caption ||
+        !body.type ||
+        !body.date ||
+        !body.time ||
+        !body.hashTages
+      )
+        return res.status(400).json({
+          ok: false,
+          message:
+            "invalid requst post content body one or more requiments not meat",
+        });
+      const filename = req.file && req.file.filename;
+      const contentBody = body;
+      //const userId = contentBody.userId;
+      const connectionId = contentBody.connectionId;
+      const hashTages = contentBody.hashTages;
+      const contentCaption = contentBody.caption;
+      const contentType = contentBody.type;
+      const contentDate = contentBody.date;
+      const contentTime = contentBody.time;
+      const postData = {
+        connectionId: connectionId,
+        engament: {
+          likes: 0,
+          comments: 0,
+          shares: 0,
+        },
+        content: {
+          type: contentType,
+          caption: contentCaption,
+          content: `http://localhost:5000/feedsContent/${filename}`,
+        },
+        engamentStates: {
+          likesId: [],
+          comments: [],
+        },
+        postId: `${connectionId}$${randomUUID()}`,
+        hashTages: [...hashTages],
+        date: contentDate,
+        time: contentTime,
+        createdAt: new Date(),
+      };
+      const createPost = new feedsPosts(postData);
+      const savePost = await createPost.save();
+      if (!savePost) throw new Error(savePost);
+      res.status(201).json({ ok: true, message: "succesful", post: postData });
+    } catch (error) {
+      res.status(500).json({ ok: false, message: `server error: ${error}` });
+    }
+  },
+);
 
 module.exports = postFeeds;
