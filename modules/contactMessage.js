792338a41e2051466103c890ee577e033a4bfe9d
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-07-09 02:04:54 +0200

    modified conection messages schema instead of keeping massages in a single doc each am adding to an array list for all messages by group

--- modules/contactMessage.js
+++ modules/contactMessage.js
@@ -1,15 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const messageSchema = new Schema({
-  connectionId: {
-    type: String,
-    required: true,
-  },
-  contactId: {
-    type: String,
-    required: true,
-  },
-  messages: {
+const messageSchema = new Schema(
+  {
     from: { type: String, required: true },
     to: { type: String, required: true },
     type: { type: String, required: true },
@@ -17,10 +9,19 @@ const messageSchema = new Schema({
     date: { type: String, required: true },
     time: { type: String, required: true },
     text: { type: String, required: false },
+    createdAt: { type: Date, default: Date.now },
   },
+  { _id: false },
+);
+const groupChatSchema = new Schema({
+  groupId: {
+    type: String,
+    required: true,
+  },
+  messages: [messageSchema],
   createdAt: { type: Date, default: Date.now },
 });
-messageSchema.index({ connectionId: 1, contactId: 1, createdAt: -1 });
+groupChatSchema.index({ groupId: 1, "messages.createdAt": 1 });
 const connnectionMessageSchema = mongoose.model(
   "connectionMassages",
   messageSchema,
