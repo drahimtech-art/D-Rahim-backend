6a9256c331f89dc8f9d49f84aae00633373c490e
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-07-13 04:16:10 +0200

    created and implimented connection requst acceptent api

--- controllers/connectionsAndChat.js
+++ controllers/connectionsAndChat.js
@@ -418,5 +418,106 @@ connectionsRouter.get(
       console.log(`server error: ${error}`);
     }
   },
+  //accept connection requst
+  connectionsRouter.post(
+    "/accept/requst/:id",
+    apiRequstValidation,
+    userValdation,
+    async (req, res) => {
+      const userId = res.tokenId;
+      const contactId = req.params.id;
+      const connectionId = req.body.connectionId;
+      try {
+        if (!contactId || !connectionId)
+          return res.status(400).json({
+            ok: false,
+            message: "invalide requst body one or more fields is missing",
+          });
+        const doesConnectionExist = await userConnections
+          .find({
+            userId: userId,
+            contactId: contactId,
+          })
+          .lean();
+        if (doesConnectionExist.length !== 0)
+          return res.status(403).json({
+            ok: false,
+            message: "Contact exist in user connections list",
+          });
+        const findContact = await userData
+          .find({ connectionId: contactId })
+          .lean();
+        if (findContact.length === 0)
+          return res
+            .status(404)
+            .json({ ok: false, message: "no user with the given id found" });
+        const connectionsRequstList = await connectionsRequst
+          .find({
+            userId: userId,
+            connectionId: connectionId,
+          })
+          .lean();
+        if (connectionsRequstList.length !== 0) {
+          // accepy requst if friend to add sent user requst
+          const requstList = connectionsRequstList[0].requstList;
+          let isResutInList;
+          let foundData;
+          const updatedAndApprovedRequstList = [];
+          for (const requst of requstList) {
+            if (requst.contactId === contactId && !requst.isConnected) {
+              isResutInList = true;
+              const connectionRequstInfo = {
+                contactId: requst.contactId,
+                chatGroupId: requst.chatGroupId,
+                invite: true,
+                isConnected: true,
+                createdAt: requst.createdAt,
+              };
+              foundData = connectionRequstInfo;
+              updatedAndApprovedRequstList.push(connectionRequstInfo);
+            } else {
+              updatedAndApprovedRequstList.push(requst);
+            }
+          }
+          if (isResutInList) {
+            const acceptRequst = await connectionsRequst.findOneAndUpdate(
+              { userId: userId, connectionId: connectionId },
+              { requstList: updatedAndApprovedRequstList },
+            );
+            const upDateFriendContactInfor =
+              await userConnections.findOneAndUpdate(
+                { userId: findContact[0]._id, contactId: connectionId },
+                { isConnected: true },
+              );
+            const addConnection = new userConnections({
+              userId: userId,
+              contactId: foundData.contactId,
+              chatGroupId: foundData.chatGroupId,
+              invite: foundData.invite,
+              isConnected: foundData.isConnected,
+              createdAt: new Date(),
+            });
+            const saveConatact = await addConnection.save();
+            if (!acceptRequst || !upDateFriendContactInfor || !saveConatact)
+              return res.status(500).json({
+                ok: false,
+                message:
+                  "server error somthing went wrong when trying to validate user connection requst",
+              });
+            return res
+              .status(200)
+              .json({ ok: true, message: "connections succesful added" });
+          }
+        }
+        return res.status(404).json({
+          ok: false,
+          message: "no connection requst found in user records",
+        });
+      } catch (error) {
+        res.status(500).json({ ok: false, message: `server error: ${error}` });
+        console.log(`server error: ${error}`);
+      }
+    },
+  ),
 );
 module.exports = connectionsRouter;
