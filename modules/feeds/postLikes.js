e9c770631707d58cae308f5fe8222661ed74d4dd
Victory Nwanoruo <nwanoruovictory87@gmail.com>
2026-07-18 06:30:49 +0200

    refactoring feeds post schema created likes schema

---
+++ modules/feeds/postLikes.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const likesSchema = new Schema({
+  postId: {
+    type: String,
+    required: true,
+  },
+  connectionId: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+likesSchema.index({ postId: 1, connectionId: 1 });
+const postLikes = mongoose.model("postLikes", likesSchema);
+module.exports = postLikes;
